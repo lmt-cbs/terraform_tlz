58600070fd3850ba8024181b6f8f68a474d3ca70
lmtbelmonte <lmtbelmonte@gmail.com>
2020-09-14 10:18:39 +0200

    Primer commit con la carga del modulo tlz de MitoGroup

---
+++ default.tfvars
@@ -0,0 +1,14 @@
+landing_zone_providers = [
+    "default"
+]
+
+landing_zone_organization_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com"
+]
+
+landing_zone_organization_enabled_policy_types = [
+    "SERVICE_CONTROL_POLICY"
+]
+
+landing_zone_organization_feature_set = "ALL"
--- main.tf
+++ main.tf
@@ -6,7 +6,20 @@ provider "aws" {
   region  = "eu-west-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "vpc_pruebas" {
-  cidr_block = "10.0.0.0/16"
+# Cargamos y ejecuamos el modulo tlz de mitogroup
+
+module "example_landing_zone_organization" {
+  source    = "../../modules/landing_zone"
+  root_path = path.module
+
+  landing_zone_providers = {
+    default = {
+      account_id = "453694400647"
+      region     = "us-east-1"
+    }
+  }
+
+  landing_zone_components = {
+    landing_zone_organization = "s3://terraform-aws-landing-zone/mycompany/landing_zone_organization/default.tfvars"
+  }
 }
